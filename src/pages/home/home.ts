d142310d29d39d068bf1dd5d3a8749cb41edc075
Thijs Smeenk <Thijs.Smeenk@topicus.nl>
2018-03-23 12:02:58 +0100

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,58 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { ListPage} from '../list/list'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  host : string;
+  name : string;
+  socket: WebSocket;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private toastCtrl: ToastController) {
+      //default dev host
+      this.host = "ws://0.0.0.0:9000"
+  }
 
+  connect() {
+    this.socket =new WebSocket(this.host);
+    this.homepageOnMessageHandler(this.socket)
+  }
+
+  homepageOnMessageHandler(socket){
+    this.socket.onmessage = (message) =>{
+        let msgAsJson = JSON.parse(message.data)
+        let type = msgAsJson['type']
+
+        switch(type){
+          case 'connected':
+            this.informAndSwitchPage(msgAsJson['payload'].run_id)
+            break
+          case 'error':
+          case 'info':
+            console.log(msgAsJson['payload'].message)
+            break
+          default:
+            console.log('No idea how to handle this message: ' + message.data)
+        }
+    }
+  }
+
+  informAndSwitchPage(runid) {
+    let toast = this.toastCtrl.create({
+      message: 'Connnected! :-D Let\'s join a run!',
+      duration: 2000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      this.navCtrl.push(ListPage, { socket : this.socket, name : this.name, runid : runid })
+    });
+
+    toast.present();
   }
 
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,134 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
+	static socket(arg0: any): any {
+		throw new Error("Method not implemented.");
+	}
   selectedItem: any;
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
+  socket: WebSocket;
+  name: string
+  runToJoin: string
+  ready: boolean
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private toastCtrl: ToastController) {
     // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    this.selectedItem = navParams.get('item')
+    this.socket = navParams.get(`socket`)
+    this.name = navParams.get(`name`)
+    this.runToJoin = navParams.get(`runid`)
+    this.setRunHandlers(this.socket)
+    this.ready = false
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    //set the user name for this session
+    this.socket.send(JSON.stringify({'type':'setname', 'payload':{ 'name': this.name}}))
 
     this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+    this.items.push({
+      title: this.name,
+      note: 'Not ready',
+      icon: 'wifi'
+    });
+
+  }
+
+  setRunHandlers(socket){
+    socket.onmessage = (message) =>{
+        let msgAsJson = JSON.parse(message.data)
+        let type = msgAsJson['type']
+
+        switch(type){
+          case 'joined':
+            this.handleJoined(msgAsJson['payload'])
+            break
+          case 'getRunnerResponse':
+            this.rebuildRunners(msgAsJson['payload'])
+            break
+          case 'runnerReadyResponse':
+            this.setRunnerReady(msgAsJson['payload'])
+          case 'error':
+          case 'info':
+            this.inform(msgAsJson['payload'].message)
+            break
+          default:
+            console.log('No idea how to handle this message: ' + message.data)
+        }
     }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
+  setRunnerReady(payload){
+    if(payload.name === this.name){
+      this.ready = payload.state
+    }
+    this.items.forEach((item, index) => {
+        if(item.title === payload.name){
+          payload.state ? item.note = 'Ready' : 'Not Ready'
+        }
+    })
+  }
+
+  rebuildRunners(payload){
+      this.items = []
+      payload.runners.forEach((item, index) => {
+        this.items.push({
+          title: item,
+          note: 'Not ready',
+          icon: 'wifi'
+        });
     });
+  }
+
+  handleJoined(payload){
+    if(payload.name === this.name){
+      this.socket.send(JSON.stringify({'type':'getRunners'}))
+    } else {
+      this.addRunner(payload.name)
+    }
+  }
+
+  reinitRunners(){
+      this.socket.send(JSON.stringify({'type':'join', 'payload':{'runtojoin':this.runToJoin}}))
+  }
+
+  addRunner(name){
+    this.items.push({
+      title: name,
+      note: 'Not ready',
+      icon: 'wifi'
+    });
+  }
+
+  inform(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+
+    });
+
+    toast.present();
+  }
+
+  joinRun(){
+    this.socket.send(JSON.stringify({'type':'join', 'payload':{'runtojoin':this.runToJoin}}))
+  }
+
+  itemTapped(event, item) {
+    if(item.title === this.name){
+      this.socket.send(JSON.stringify({'type':'runnerReady', 'payload':{'name':item.title, 'state':!this.ready}}))
+    } else {
+      this.inform('You cannot force any other runners to be ready!')
+    }
+
   }
 }
